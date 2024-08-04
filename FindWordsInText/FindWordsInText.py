0e4a13daedd639d3aaf348dfb0462e74300b498f
Nobodyitw <123703598+Shrider1@users.noreply.github.com>
2024-08-05 00:03:10 +0300

    Update FindWordsInText.py

--- FindWordsInText/FindWordsInText.py
+++ FindWordsInText/FindWordsInText.py
@@ -17,9 +17,7 @@ def Readerwords(inpath):
     while i != len(fileread): # Пока i не равно количеству символов содержимого.
         if fileread[i] == " ": # Если находим пробел.
             last.append(i+1) # Добавляем его айди в список.
-
         i += 1
-
     last.append(len(fileread)) # Добавляем кол символов в список с индексом пробелов. Исключает ошибку с неверным индексом.
     while ii != len(last): # Пока ii не равно кол индексов пробела.
         mass.append(fileread[last[first]:(last[second]-1)]) # Добавить в список mass символы от first до second-1. -1 для исключения последнего символа. Им являеться пробел.
